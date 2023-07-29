b9af9a88cbdcee35490b5ca2626835181fb72095
Éloi Strée <eloistree@gmail.com>
2023-07-29 22:28:54 +0200

    save

---
+++ Samples~/PlatformNeutral/#SshCloneAll.bat
@@ -0,0 +1,22 @@
+rmdir GitRef /s /q
+mkdir GitRef
+cd GitRef
+
+:: Toolboxes that I use and make evolved with all my project. Some is dirty some are good.
+git clone git@github.com-EloiStree:EloiStree/2021_10_03_EloiGeneralToolbox.git
+:: Want to learn what is the project OMI and invest on it ?
+git clone git@github.com-OpenMacroInput:OpenMacroInput/LearnAndInvestOnTheProject.git
+
+:: This tool allows me to build and modify package for the package manager
+git clone git@github.com-EloiStree:EloiStree/2019_07_21_UnityPackageFacilitator.git
+
+:: This Git utility allows me to accelerate the commit from Unity
+git clone git@github.com-EloiStree:EloiStree/2019_07_21_QuickGitUtility.git
+
+:: This repository in a tag to say that I was in the project.
+git clone git@github.com-EloiStree:EloiStree/GeneralLicense.git
+
+
+
+echo "Done"
+pause
